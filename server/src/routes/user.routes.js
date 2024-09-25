70ed970e68896981a154fb7e9511d3d540c72b1e
warissayed <warissayed72@gmail.com>
2024-09-25 20:05:07 +0530

    Added multer middleware for file transfer

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -6,6 +6,7 @@ import {
   logoutUser,
   registerUser,
 } from "../controllers/user.controllers.js";
+import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
 
@@ -13,7 +14,15 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logoutUser);
 router.route("/profile").get(isUserLoggedIn);
-router.route("/CreatePost").post(createPost);
+router.route("/CreatePost").post(
+  upload.fields([
+    {
+      name: "file",
+      maxCount: 1,
+    },
+  ]),
+  createPost
+);
 console.log("Hello from router");
 
 export default router;
