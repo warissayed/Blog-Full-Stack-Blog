9cb0eb22e9f9d5c3f68903238528684a99dc35d9
Waris sayed <119447310+warissayed@users.noreply.github.com>
2024-11-27 04:30:59 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://xzenblog.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
